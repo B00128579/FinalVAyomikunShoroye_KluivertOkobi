8e6bae0356e0ff81929113bac6fb9f60ab3d1f4b
B00128579 <71762776+B00128579@users.noreply.github.com>
2021-12-13 08:50:09 +0000

    F

---
+++ Assets/_Scripts/OpenLink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink :MonoBehaviour
+{
+	public void OpenChannel()
+	{
+
+		Application.OpenURL("https://github.com/B00128579/AlphaVShoroye_Okobi/tree/main/Assets/_Scripts");
+	}
+
+}
